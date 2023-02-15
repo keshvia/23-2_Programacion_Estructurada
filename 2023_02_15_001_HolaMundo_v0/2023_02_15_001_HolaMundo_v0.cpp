01f671958cb71a9819d6f7e1cbf63951595f94a1
Karen Dayanare <124415841+keshvia@users.noreply.github.com>
2023-02-15 09:10:43 -0600

    0002 - Cambios
    Solo subiré los cambios que hice aquí.

---
+++ 2023_02_15_001_HolaMundo_v0/2023_02_15_001_HolaMundo_v0.cpp
@@ -0,0 +1,20 @@
+// 2023_02_15_001_HolaMundo_v0.cpp :
+// Este archivo es mi primer programa.
+// Dayanare Ruiz ZAP870
+
+// Aquí pondré las bibliotecas necesarias.
+#include <iostream>
+#include <locale.h>
+
+int main()
+{   
+    setlocale(LC_CTYPE, "spanish");
+
+
+    std::cout << "¡Holi!\n";
+    std::cout << "Soy Daya lol\n";
+    std::cout << "Este es un salto de línea y la ñ.\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
